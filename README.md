d7257e55e186c9a684531962a0b1cd73632ec28b
Vadim Kantorov <vadimkantorov@gmail.com>
2025-07-10 12:57:46 +0200

    ...

--- README.md
+++ README.md
@@ -1,1 +1,59 @@
 # torchlisttensors
+- traverses the autograd backward graph and prints all saved-for-backward tensors and leafs
+- starting from a provided root
+- also tries to discover gc-tracked alive, and non gc-tracked, and gc-tracked non-alive using `gc.get_objects()` and `sys.getobjects()`. Currently (in python <= 3.14) `sys.getobjects()` is only available only when python is built with `--with-trace-refs` build option - according to the Python Discuss thread linked below
+- useful for memory/OOM debugging
+
+## References
+- https://github.com/pytorch/pytorch/issues/91692 - the main ask/discussion for inclusion of this utility in PyTorch core
+- https://github.com/pytorch/pytorch/issues/104247
+- https://discuss.python.org/t/list-all-objects-gc-tracked-or-not-of-a-given-type-for-debugging-introspection-and-memory-profiling
+- https://www.mail-archive.com/numpy-discussion@scipy.org/msg46120.html
+- https://github.com/szagoruyko/pytorchviz/blob/0adcd83af8aa7ab36d6afd139cabbd9df598edb7/torchviz/dot.py#L146
+
+## Example usage:
+```python
+import torch
+
+import torchlisttensors
+    
+model = torch.nn.Sequential(torch.nn.Linear(20, 20), torch.nn.Linear(20, 20), torch.nn.Linear(20, 20))
+model = torchlisttensors.assign_names(model, 'model')
+
+x = torch.zeros(4, 20)
+x.__name__ = 'x'
+
+loss = model(x).sum()
+loss.__name__ = 'loss'
+
+z = torch.zeros(65, 35)
+z.__name__ = 'z'
+
+tensors = torchlisttensors.torchlisttensors(loss)
+for i, t in tensors.items():
+    print(i, '\t', t)
+```
+
+```
+140022603754192          shape=(20,20)  name=model.0.weight
+140022604499456          shape=(20)     name=model.0.bias
+140022604499376          shape=(20,20)  name=model.1.weight
+140022604499536          shape=(20)     name=model.1.bias
+140022604499696          shape=(20,20)  name=model.2.weight
+140022604499616          shape=(20)     name=model.2.bias
+140022606243040          shape=(4,20)   name=x
+140022604499776          shape=()       name=loss
+140022604500336          shape=(65,35)  name=z
+140022604500256          shape=(4,20)   name=mat1_AddmmBackward0_140021819231392_SAVED
+140022604506656          shape=(20,20)  name=mat2_AddmmBackward0_140021819231392_SAVED
+140022604500176          shape=(4,20)   name=mat1_AddmmBackward0_140021821040880_SAVED
+140022604507136          shape=(20,20)  name=mat2_AddmmBackward0_140021821040880_SAVED
+```
+
+Also a nasty warning is printed:
+```
+/home/vadimkantorov/.local/lib/python3.12/site-packages/torch/__init__.py:1028: FutureWarning: `torch.distributed.reduce_op` is deprecated, please use `torch.distributed.ReduceOp` instead
+  return isinstance(obj, torch.Tensor)
+/mnt/c/Users/vadim/torchlisttensors/torchlisttensors.py:112: FutureWarning: `torch.distributed.reduce_op` is deprecated, please use `torch.distributed.ReduceOp` instead
+  elif hasattr(obj, 'data') and torch.is_tensor(obj.data):
+```
---
+++ torchlisttensors.py
@@ -0,0 +1,152 @@
+import gc
+import sys
+
+import torch
+    
+def torchlisttensors(*roots):
+    # https://github.com/szagoruyko/pytorchviz/blob/0adcd83af8aa7ab36d6afd139cabbd9df598edb7/torchviz/dot.py#L146
+
+    SAVED_PREFIX = "_saved_"
+    seen = set()
+    nodes = {}
+
+    def found_tensor_hook(tensor, name):
+        if id(tensor) not in nodes:
+            nodes[id(tensor)] = name
+    
+    def get_var_name(var, name = '', from_var = None):
+        if not name:
+            #name = param_map[id(var)] if id(var) in param_map else ''
+            if hasattr(var, '__name__'):
+                name = var.__name__
+            else:
+                name = '__' + str(id(var)) + '__'
+        return 'shape=({})\tname={}'.format(','.join(map(str, var.shape)), name)
+
+    def get_fn_name(fn, show_attrs, max_attr_chars):
+        name = str(type(fn).__name__)
+        if not show_attrs:
+            return name
+        attrs = dict()
+        for attr in dir(fn):
+            if not attr.startswith(SAVED_PREFIX):
+                continue
+            val = getattr(fn, attr)
+            attr = attr[len(SAVED_PREFIX):]
+            if torch.is_tensor(val):
+                attrs[attr] = "[saved tensor]"
+            elif isinstance(val, tuple) and any(map(torch.is_tensor, val)):
+                attrs[attr] = "[saved tensors]"
+            else:
+                attrs[attr] = str(val)
+        if not attrs:
+            return name
+        max_attr_chars = max(max_attr_chars, 3)
+        col1width = max(map(len, attrs.keys()))
+        col2width = min(max(map(len, map(str, attrs.values()))), max_attr_chars)
+        
+        attrstr = '%-' + str(col1width) + 's: %' + str(col2width)+ 's'
+        truncate = lambda s: s[:col2width - 3] + "..." if len(s) > col2width else s
+        params = '\n'.join(attrstr % (k, truncate(str(v))) for (k, v) in attrs.items())
+        
+        sep = "-" * max(col1width + col2width + 2, len(name))
+        return name + '\n' + sep + '\n' + params
+
+    def add_nodes(fn, from_var = None):
+        assert not torch.is_tensor(fn)
+        if fn in seen:
+            return
+        seen.add(fn)
+
+        fmtvarname = lambda varname, saved = False: varname + '_{}_{}'.format(type(fn).__name__, from_var) + ('_SAVED' * saved)
+
+        if True:
+            for attr in dir(fn):
+                if not attr.startswith(SAVED_PREFIX):
+                    continue
+                val = getattr(fn, attr)
+                seen.add(val)
+                attr = attr[len(SAVED_PREFIX):]
+                if torch.is_tensor(val):
+                    varname = get_var_name(val, attr, from_var = id(fn) )
+                    found_tensor_hook(val, fmtvarname(varname, saved = True))
+                if isinstance(val, tuple):
+                    for i, t in enumerate(val):
+                        if torch.is_tensor(t):
+                            varname = get_var_name(t, attr + '[{}]'.format(i), from_var = id(fn))
+                            found_tensor_hook(t, fmtvarname(varname, saved = True))
+
+        if hasattr(fn, 'variable'):
+            var = fn.variable
+            seen.add(var)
+            varname = get_var_name(var, from_var = id(fn))
+            found_tensor_hook(var, fmtvarname(varname))
+
+        if hasattr(fn, 'next_functions'):
+            for u in fn.next_functions:
+                if u[0] is not None:
+                    add_nodes(u[0], from_var = id(fn))
+
+        if hasattr(fn, 'saved_tensors'):
+            for t in fn.saved_tensors:
+                seen.add(t)
+                varname = get_var_name(t, from_var = id(fn))
+                found_tensor_hook(t, fmtvarname(varname))
+
+    def add_base_tensor(var):
+        if var in seen:
+            return
+        seen.add(var)
+        found_tensor_hook(var, get_var_name(var))
+        if var.grad_fn:
+            add_nodes(var.grad_fn, from_var = id(var))
+        if var._is_view():
+            add_base_tensor(var._base, from_var = id(var))
+
+    seeds = roots[:]
+    for objs in [gc.get_objects(), sys.getobjects() if hasattr(sys, 'getobjects') else []]:
+        for obj in objs:
+            try:
+                if torch.is_tensor(obj):
+                    tensor = obj
+                elif hasattr(obj, 'data') and torch.is_tensor(obj.data):
+                    tensor = obj.data
+                else:
+                    continue
+                if hasattr(tensor, '__name__'):
+                    found_tensor_hook(tensor, get_var_name(tensor))
+                elif tensor not in seeds:
+                    seeds.append(tensor)
+            except Exception as e:
+                pass
+    
+    for tensor in seeds:
+        add_base_tensor(tensor)
+
+    return nodes
+
+def assign_names(model, name):
+    for __name__, m in model.named_modules(prefix = name):
+        m.__name__ = __name__
+    for __name__, p in model.named_parameters(prefix = name):
+        p.__name__ = __name__
+    for __name__, b in model.named_buffers(prefix = name):
+        b.__name__ = __name__
+    return model
+
+if __name__ == '__main__':
+    model = torch.nn.Sequential(torch.nn.Linear(20, 20), torch.nn.Linear(20, 20), torch.nn.Linear(20, 20))
+    model = assign_names(model, 'model')
+
+    x = torch.zeros(4, 20)
+    x.__name__ = 'x'
+
+    loss = model(x).sum()
+    loss.__name__ = 'loss'
+
+    z = torch.zeros(65, 35)
+    z.__name__ = 'z'
+
+    tensors = torchlisttensors(loss)
+    for i, t in tensors.items():
+        print(i, '\t', t)
